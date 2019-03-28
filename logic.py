40d5be22575b036fdb3860cad788f6aa637eded0
zhouxiaoapps <zhouxiaoapps@gmail.com>
2019-03-28 16:18:44 +0800

    add by2

--- logic.py
+++ logic.py
@@ -1,3 +1,14 @@
+import redis
+
+rd = redis.StrictRedis(host='redis', port=6379)
+KEY = 'visit'
+
+
 def pick_bucket(num: int, bucket_num: int = 2) -> int:
     assert bucket_num > 0
     return num % bucket_num
+
+
+def auto_incr() -> int:
+    rd.incrby(KEY, 2)
+    return int(rd.get(KEY))
