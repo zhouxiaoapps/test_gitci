6f6543810f3e61a8c51bb0b3476ce8a7e0f190bd
zhouxiaoapps <zhouxiaoapps@gmail.com>
2019-03-28 18:07:18 +0800

    init incr3

--- logic.py
+++ logic.py
@@ -1,3 +1,15 @@
+import redis
+
+
 def pick_bucket(num: int, bucket_num: int = 2) -> int:
     assert bucket_num > 0
     return num % bucket_num
+
+
+rd = redis.StrictRedis(host='redis', port=6379)
+KEY = 'visit'
+
+
+def auto_incr() -> int:
+    rd.incrby(KEY, 2)
+    return int(rd.get(KEY))
